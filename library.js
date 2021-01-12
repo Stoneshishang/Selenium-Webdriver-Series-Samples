b09c3f55ed7d690fbfd8cdcf196256ed2f87503c
Shang Shi <shangs@XinningNinas-MacBook-Pro.local>
2021-01-11 21:22:48 -0500

    use async await to replace drive.sleep()

--- library.js
+++ library.js
@@ -10,17 +10,25 @@ const webdriver =  require('selenium-webdriver'),
 
       driver.get('https://library-app.firebaseapp.com/');
       
-      driver.findElement(By.css('input'));
-      driver.findElement(By.css('.btn-primary')).getText().then((txt)=>{
-        console.log("Text of the btn is " + txt);
-      });
-      driver.findElements(By.css('nav li')).then((elements)=>{
-        console.log('elements are: ' + elements);
-        elements.map((el)=>{
-          el.getText().then((txt)=>{
-            console.log('the text of the navbar element is: ' + txt);
-          })
+      driver.findElement(By.css('input')).sendKeys('user@email.com');
+      driver.findElement(By.css('.btn-primary')).click();
+      // driver.findElements(By.css('nav li')).then((elements)=>{
+      //   elements.map((el)=>{
+      //     el.getText().then((txt)=>{
+      //       console.log('the text of the navbar element is: ' + txt);
+      //     })
+      //   })
+      // });
+
+      async function message(){
+        await driver.wait(until.elementsLocated(By.css('.alert-success'),10000));
+        await driver.findElement(By.css('.alert')).getText().then((txt)=>{
+          console.log("Alert success text is: " + txt );
         })
-      });
+      }
+
+      message();
+
+      
       
 
